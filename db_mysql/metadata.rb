6a5a1b649b2306487c292983930e4da9813755a9
kantoo <keren@la-mark.com>
2013-02-04 03:14:25 -0800

    Create metadata.rb

---
+++ db_mysql/metadata.rb
@@ -0,0 +1,6 @@
+maintainer "RightScale, Inc."
+maintainer_email "keren@la-mark.com"
+license "RightScale Licence"
+description "Install customer my.cnf file"
+long_description "Long Description"
+version "0.0.1"
